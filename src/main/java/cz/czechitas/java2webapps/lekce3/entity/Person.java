4e24dc8781b6f2cb184f32f66926453bc9b15f71
alzbetahladikova <beta.strelbova@gmail.com>
2023-10-03 18:17:24 +0200

    založení osoby

---
+++ src/main/java/cz/czechitas/java2webapps/lekce3/entity/Person.java
@@ -0,0 +1,48 @@
+package cz.czechitas.java2webapps.lekce3.entity;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+    private String givenName;
+    private String surname;
+    private LocalDate birthDate;
+    private String photosURL;
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhotosURL() {
+        return photosURL;
+    }
+
+    public void setPhotosURL(String photosURL) {
+        this.photosURL = photosURL;
+    }
+
+    public int getAge() {
+        Period period = birthDate.until(LocalDate.now());
+    return period.getYears();}
+
+}
